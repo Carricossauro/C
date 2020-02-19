d2ff944e433b64bcc6361e1046f0f9eff57a8c9e
tiagocarrico27@gmail.com <tiagocarrico27@gmail.com>
2020-02-19 17:58:56 +0000

    'Mensagem'

--- LAFT2_T1/principal.c
+++ LAFT2_T1/principal.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+#include <assert.h>
 #include "indicadores.h"
 
 int main() {
     float peso, altura, indice;
+
     printf("Peso (Kg): ");
     scanf("%f", &peso);
+
     printf("Altura (m): ");
     scanf("%f", &altura);
+
+    assert( (peso > 40 && altura > 1.4) && "Valores muito baixos!");
+    assert( (peso < 200 && altura < 2.3) && "Valores muito altos!");
+
     indice = imc(peso, altura);
     printf("O seu IMC é %f.\n", indice);
+
     return 0;
 }
