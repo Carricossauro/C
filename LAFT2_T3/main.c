cb6082fbc50fdcbfcbe4bf16e288a1a074755f5a
tiagocarrico27@gmail.com <tiagocarrico27@gmail.com>
2020-02-20 14:53:55 +0000

    nsfvojnslk

--- LAFT2_T3/main.c
+++ LAFT2_T3/main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
-void certo();
-void errado();
 void op(char o);
 int resultado(int x, int y, char o);
 void rando(int *x, int *y, int d);
@@ -11,7 +10,6 @@ void rando(int *x, int *y, int d);
 int main() {
     while (1) {
         char j;
-
         printf("Escolha a operação aritmética: ");
         scanf(" %c", &j);
 
@@ -19,73 +17,11 @@ int main() {
     }
 }
 
-void certo() {
-    srand(time(NULL));
-    int n = rand() % 5;
-
-    switch (n) {
-        case 0:
-            printf("Acertaste.\n");
-            break;
-        case 1:
-            printf("Bem jogado!\n");
-            break;
-        case 2:
-            printf("Contas bem feitas!\n");
-            break;
-        case 3:
-            printf("Parabéns! Acertaste!\n");
-            break;
-        case 4:
-            printf("Está certo. Muito bem!\n");
-            break;
-        default:
-            break;
-    }
-}
-
-void errado() {
-    srand(time(NULL));
-    int n = rand() % 5;
-
-    switch (n) {
-        case 0:
-            printf("Não.\n");
-            break;
-        case 1:
-            printf("Errado.\n");
-            break;
-        case 2:
-            printf("Tenta outra vez.\n");
-            break;
-        case 3:
-            printf("Não está certo. :(\n");
-            break;
-        case 4:
-            printf("Não desistas! Tenta outra vez.\n");
-            break;
-        default:
-            break;
-    }
-}
-
 void rando(int *x, int *y, int d) {
     srand(time(NULL));
 
-    switch (d) {
-        case 1:
-            *x = 1 + rand() % 100;
-            *y = 1 + rand() % 100;
-            break;
-        case 2:
-            *x = 1 + rand() % 1000;
-            *y = 1 + rand() % 1000;
-            break;
-        default:
-            *x = 1 + rand() % 10;
-            *y = 1 + rand() % 10;
-            break;
-    }
+    *x = rand() % ((int)(pow(10, d)));
+    *y = rand() % ((int)(pow(10, d)));
 }
 
 int resultado(int x, int y, char o) {
@@ -98,19 +34,26 @@ int resultado(int x, int y, char o) {
 }
 
 void op(char o) {
-    int x, y, d, resposta;
+    int x, y, d, resposta, cr;
     printf("Escolhe a dificuldade: ");
     scanf(" %d", &d);
-    rando(&x, &y, d);
 
     while (1) {
-        printf("Quanto é %d %c %d", x, o, y);
-        scanf(" %d", &resposta);
+        rando(&x, &y, d);
 
-        if (resposta == resultado(x, y, o)) {
-            certo();
-            break;
-        } else errado();
+        char certas[5][50] = {"Acertaste.\n","Bem jogado!\n","Contas bem feitas!\n","Parabéns! Acertaste!\n","Está certo. Muito bem!\n"};
+        char erradas[5][50] = {"Não.\n","Errado.\n","Tenta outra vez.\n","Não está certo. :(\n","Não desistas! Tenta outra vez.\n"};
+
+        while (1) {
+            printf("Quanto é %d %c %d? ", x, o, y);
+            scanf(" %d", &resposta);
+
+            int result = resultado(x, y, o);
+            srand(time(NULL));
+            if (resposta == result) {
+                printf("%s", certas[rand() % 5]);
+                break;
+            } else printf("%s", erradas[rand() % 5]);
+        }
     }
-
 }
