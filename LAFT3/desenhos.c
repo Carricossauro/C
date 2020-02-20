cf6a70318b35a62b218b3982958ebfe1d4516b0c
tiagocarrico27@gmail.com <tiagocarrico27@gmail.com>
2020-02-20 17:36:27 +0000

    LAFT3

--- LAFT3/desenhos.c
+++ LAFT3/desenhos.c
@@ -1,5 +1,47 @@
+#include <stdio.h>
 #include "desenhos.h"
 
 void imprime_triangulo(int num_linhas) {
+    int linha = 1;
 
+    for (int i = 0; i < num_linhas; i++) {
+        imprimeLinha(linha++, num_linhas);
+        putchar('\n');
+    }
+}
+
+void imprime_losango(int num_linhas) {
+    int linha = 1, i;
+
+    for (i = 0; i < num_linhas; i++) {
+        imprimeLinha(linha++, num_linhas);
+        putchar('\n');
+    }
+    for (; i > 0; i--) {
+        imprimeLinha(--linha, num_linhas);
+        putchar('\n');
+    }
+}
+
+void imprimeLinha(int linha, int nl) {
+    char ar[nl];
+    int x = nl - linha, ca = 65, i;
+
+    //Guarda as letras ou espaçoes nos arrays
+    for (i = 1; i <= x; i++)
+        ar[i] = ' ';
+
+    for (;i <= nl; i++)
+        ar[i] = ca++;
+
+    //Prints
+    for (i = 1; i <= nl; i++) {
+        printf("%c", ar[i]);
+        putchar(' ');
+    }
+
+    for (i = nl - 1; i >= 0; i--) {
+        printf("%c", ar[i]);
+        putchar(' ');
+    }
 }
--- LAFT3/desenhos.h
+++ LAFT3/desenhos.h
@@ -6,5 +6,8 @@
 #define LAFT3_DESENHOS_H
 
 void imprime_triangulo(int num_linhas);
+void imprimeLinha(int linha, int nl);
+void imprime_losango(int num_linhas);
+
 
 #endif //LAFT3_DESENHOS_H
