f9fbc8fe18728243234d4f271efc276716e101b7
tiagocarrico27@gmail.com <tiagocarrico27@gmail.com>
2020-02-21 14:49:43 +0000

    LAFT3 final

--- LAFT3/desenhos.c
+++ LAFT3/desenhos.c
@@ -12,13 +12,12 @@ void imprime_triangulo(int num_linhas) {
 }
 
 void imprime_losango(int num_linhas) {
-    int linha = 1, i;
+    int linha = 0, i;
 
     for (i = 0; i < num_linhas; i++) {
-        imprimeLinha(linha++, num_linhas);
+        imprimeLinha(++linha, num_linhas);
         putchar('\n');
     }
-    linha--;
     for (; i > 0; i--) {
         imprimeLinha(--linha, num_linhas);
         putchar('\n');
@@ -26,20 +25,19 @@ void imprime_losango(int num_linhas) {
 }
 
 void imprimeLinha(int linha, int nl) {
-    int ca = 65;
+    int ca = 64;
     for (int i = 0; i < 2 * (nl - linha); i++)
         putchar(' ');
 
     for (int i = 0; i < linha; i++)
-        printf("%c ", ca++);
-    ca--;
+        printf("%c ", ++ca);
+
     for (int i = 0; i < linha - 1; i++)
         printf("%c ", --ca);
 }
 
 void imprime_hexagono(int num) {
     prim_ult(num);
-
     for (int l = 1; l <= num; l++) {
         for (int i = 0; i < num - l; i++)
             putchar(' ');
@@ -56,25 +54,20 @@ void imprime_hexagono(int num) {
     for (int l = num - 1; l >= 1; l--) {
         for (int i = 0; i < num - l; i++)
             putchar(' ');
-
         putchar('#');
 
         for (int i = 0; i < num + (2 * (l - 1));i++)
             putchar(' ');
-
         putchar('#');
         putchar('\n');
     }
-
     prim_ult(num);
 }
 
 void prim_ult(int n) {
-    for (int i = 0; i < n; i++)
-        putchar(' ');
-
-    for (int i = 0; i < n; i++)
-        putchar('#');
-
+    for (int i = 0; i < n * 2; i++) {
+        if (i >= n) putchar('#');
+        else putchar(' ');
+    }
     putchar('\n');
 }
