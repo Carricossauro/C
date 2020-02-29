ca20317381704447b1c96aa51a3682577545c8cd
tiagocarrico27@gmail.com <tiagocarrico27@gmail.com>
2020-02-29 12:30:40 +0000

    LAFT4

---
+++ LAFT4/cmake-build-debug/CMakeFiles/LAFT4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LAFT4.dir/main.c.o"
+  "LAFT4"
+  "LAFT4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LAFT4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ LAFT4/main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void soma_elemento(int *arr, int dim, int idx);
+void roda_esq(int *arr, int dim, int shifter);
+int remove_menores(int *arr, int dim, int valor);
+
+int main() {
+    int n, i, *arr, ex;
+    printf("Exercício: ");
+    scanf("%d", &ex);
+    printf("Tamanho do array: ");
+    scanf("%d", &n);
+    arr = malloc(n * sizeof(int));
+    printf("Array: \n");
+    for (int o = 0; o < n; o++) scanf("%d", &arr[o]);
+    if (ex == 1) {
+        printf("Indice: ");
+        scanf("%d", &i);
+        soma_elemento(arr, n, i);
+        for (int m = 0; m < n; m++) printf("%d ", arr[m]);
+    } else if (ex == 2) {
+        printf("Shifter: ");
+        scanf("%d", &i);
+        roda_esq(arr, n, i);
+        for (int m = 0; m < n; m++) printf("%d ", arr[m]);
+    } else if (ex == 3) {
+        printf("Valor: ");
+        scanf("%d", &i);
+        remove_menores(arr, n, i);
+        for (int m = 0; m < n; m++) printf("%d ", arr[m]);
+    }
+    return 0;
+}
+
+void soma_elemento(int *arr, int dim, int idx) {
+    int num;
+    if (idx < dim) {
+        num = arr[idx];
+        for (int i = 0; i < dim; i++) arr[i] = arr[i] + num;
+    }
+}
+
+void roda_esq(int *arr, int dim, int shifter) {
+    while (shifter >= dim) {
+        shifter -= dim;
+    }
+
+    int *fim = malloc(shifter * sizeof(int));
+    for (int i = 0; i < shifter; i++) fim[i] = arr[i];
+
+    int pos = 0;
+    for (int i = shifter; i < dim; i++) arr[pos++] = arr[i];
+    for (int i = 0; i < shifter; i++) arr[pos++] = fim[i];
+}
+
+int remove_menores(int *arr, int dim, int valor) {
+    int *fim, posFim = 0;
+    fim = malloc(dim * sizeof(int));
+    for (int i = 0; i < dim; i++) if (arr[i] < valor) fim[posFim++] = arr[i];
+
+    int pos = 0;
+    for (int i = 0; i < dim; i++) if (arr[i] >= valor) arr[pos++] = arr[i];
+    for(int i = 0; i < posFim; i++) arr[pos++] = fim[i];
+}
